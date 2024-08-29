93f04b22624cd3746c509dc4cca90176e5abc909
Maxime <maxime.guigra@gmail.com>
2024-08-29 23:24:02 +0200

    feat: advancement header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
-    <div className="flex h-12 bg-lime-700">
-      <ul className="flex gap-3">
-        <li>
-          <Link to="/">Create Employee</Link>
-        </li>
-        <li>
-          <Link to="/about">View Current Employees</Link>
-        </li>
-      </ul>
+    <div className="flex items-center justify-between bg-lime-700">
+      <div>
+        <ul className="flex gap-3 ml-4 ">
+          <li>
+            <Link to="/">Create Employee</Link>
+          </li>
+          <li>
+            <Link to="/about">View Current Employees</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <img src={logo} alt="" />
+      </div>
     </div>
   );
 }
