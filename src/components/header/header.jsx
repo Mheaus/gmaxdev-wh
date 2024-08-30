37c0ee40c7535ba29f6dc59ea5d232f3d517c98e
GMaxDev <maxime.guigra@gmail.com>
2024-08-30 10:14:02 +0200

    feat: header advancement

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,14 +3,14 @@ import logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between bg-lime-700">
+    <div className="flex items-center justify-between bg-lime-700 text-white">
       <div>
-        <ul className="flex gap-3 ml-4 ">
+        <ul className="flex gap-3 ml-4 items-center">
           <li>
             <Link to="/">Create Employee</Link>
           </li>
           <li>
-            <Link to="/about">View Current Employees</Link>
+            <Link to="/about">View Current <br/>Employees</Link>
           </li>
         </ul>
       </div>
--- src/pages/createEmployee/createEmployee.jsx
+++ src/pages/createEmployee/createEmployee.jsx
@@ -1,7 +1,62 @@
 export default function CreateEmployee() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">EMPLOYEE!</h1>
-    </>
+    <div className="flex flex-col items-center">
+      <section className="flex flex-col items-center p-6 bg-gray-300 rounded-2xl mt-10 w-2/4">
+        <div className="flex gap-10 mb-4">
+          <div className="flex">
+            <p>First Name</p>
+            <input className="border" type="text" />
+          </div>
+
+          <div className="flex">
+            <p>Last Name</p>
+            <input className="border" type="text" />
+          </div>
+        </div>
+        <div className="flex gap-10">
+          <div className="flex">
+            <p>Date of Birth</p>
+            <input className="border" type="text" />
+          </div>
+
+          <div className="flex">
+            <p>Start Date</p>
+            <input className="border" type="text" />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col p-6 bg-gray-300 rounded-2xl mt-10 items-center w-2/4">
+        <div className="flex gap-10 mb-4">
+          <div className="flex">
+            <p>Street</p>
+            <input className="border" type="text" />
+          </div>
+
+          <div className="flex">
+            <p>City</p>
+            <input className="border" type="text" />
+          </div>
+        </div>
+        <div className="flex gap-10">
+          <div className="flex">
+            <p>State</p>
+            <input className="border" type="text" />
+          </div>
+
+          <div className="flex">
+            <p>Zip Code</p>
+            <input className="border" type="text" />
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col p-6 bg-gray-300 rounded-2xl mt-10 items-center w-2/4">
+        <div className="flex gap-10 mb-4">
+          <div className="flex">
+            <p>Department</p>
+            <input className="border" type="text" />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
