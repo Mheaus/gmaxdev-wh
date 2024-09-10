1b8c5d63213b0196435c19833148a927e2cbfbd1
GMaxDev <maxime.guigra@gmail.com>
2024-09-10 11:46:28 +0200

    feat: viewCurrentEmployee progress + visual update createEmployee

--- apps/front/src/pages/createEmployee/createEmployee.jsx
+++ apps/front/src/pages/createEmployee/createEmployee.jsx
@@ -1,60 +1,53 @@
 export default function CreateEmployee() {
   return (
     <div className="flex flex-col items-center">
-      <section className="flex flex-col items-center p-6 bg-gray-300 rounded-2xl mt-10 w-2/4">
-        <div className="flex gap-10 mb-4">
-          <div className="flex">
-            <p>First Name</p>
-            <input className="border" type="text" />
-          </div>
 
-          <div className="flex">
-            <p>Last Name</p>
-            <input className="border" type="text" />
+      <section className="w-2/4 p-6 mt-10 bg-gray-300 rounded-2xl">
+        <div className="flex flex-wrap gap-6 mb-4">
+          <div className="flex items-center">
+            <p className="w-32 mr-4">First Name</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
-        </div>
-        <div className="flex gap-10">
-          <div className="flex">
-            <p>Date of Birth</p>
-            <input className="border" type="text" />
+          <div className="flex items-center">
+            <p className="w-32 mr-4">Last Name</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
-
-          <div className="flex">
-            <p>Start Date</p>
-            <input className="border" type="text" />
+          <div className="flex items-center">
+            <p className="w-32 mr-4">Date of Birth</p>
+            <input className="px-2 py-1 border" type="text" />
+          </div>
+          <div className="flex items-center">
+            <p className="w-32 mr-4">Start Date</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
         </div>
       </section>
-      <section className="flex flex-col p-6 bg-gray-300 rounded-2xl mt-10 items-center w-2/4">
-        <div className="flex gap-10 mb-4">
-          <div className="flex">
-            <p>Street</p>
-            <input className="border" type="text" />
-          </div>
 
-          <div className="flex">
-            <p>City</p>
-            <input className="border" type="text" />
+      <section className="w-2/4 p-6 mt-10 bg-gray-300 rounded-2xl">
+        <div className="flex flex-wrap gap-6 mb-4">
+          <div className="flex items-center">
+            <p className="w-32 mr-4">Street</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
-        </div>
-        <div className="flex gap-10">
-          <div className="flex">
-            <p>State</p>
-            <input className="border" type="text" />
+          <div className="flex items-center">
+            <p className="w-32 mr-4">City</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
-
-          <div className="flex">
-            <p>Zip Code</p>
-            <input className="border" type="text" />
+          <div className="flex items-center">
+            <p className="w-32 mr-4">State</p>
+            <input className="px-2 py-1 border" type="text" />
+          </div>
+          <div className="flex items-center">
+            <p className="w-32 mr-4">Zip Code</p>
+            <input className="px-2 py-1 border" type="text" />
           </div>
         </div>
       </section>
-      <section className="flex flex-col p-6 bg-gray-300 rounded-2xl mt-10 items-center w-2/4">
-        <div className="flex gap-10 mb-4">
-          <div className="flex">
-            <p>Department</p>
-            <input className="border" type="text" />
-          </div>
+
+      <section className="w-2/4 p-6 mt-10 bg-gray-300 rounded-2xl">
+        <div className="flex items-center w-full">
+          <p className="w-32 mr-4">Department</p>
+          <input className="flex-grow px-2 py-1 border" type="text" />
         </div>
       </section>
     </div>
--- apps/front/src/pages/viewCurrentEmployee/viewCurrentEmployee.jsx
+++ apps/front/src/pages/viewCurrentEmployee/viewCurrentEmployee.jsx
@@ -1,7 +1,89 @@
+import { useState } from "react";
+
 export default function ViewEmployee() {
+  const [employees, setEmployees] = useState([
+    {
+      firstName: "John",
+      lastName: "Doe",
+      startDate: "2022-05-01",
+      department: "HR",
+      birthDate: "1990-06-12",
+      street: "123 Main St",
+      city: "New York",
+      state: "NY",
+      zipCode: "10001",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      startDate: "2023-03-15",
+      department: "Finance",
+      birthDate: "1985-02-24",
+      street: "456 Elm St",
+      city: "Los Angeles",
+      state: "CA",
+      zipCode: "90001",
+    },
+  ]);
+
   return (
-    <div className="flex flex-col items-center">
-      <h1>toto</h1>
+    <div className="flex flex-col items-center mx-20 my-10">
+      <div className="flex justify-between w-full">
+        <div>
+          Show <input id="entie_show" className="w-10 border" type="text" /> entries
+        </div>
+        <div>
+          Search <input className="border" type="text" />
+        </div>
+      </div>
+      <div className="w-full">
+        <table className="w-full mt-5 bg-gray-300 rounded-md">
+          <thead>
+            <tr className="bg-gray-400">
+              {[
+                "First Name",
+                "Last Name",
+                "Start Date",
+                "Department",
+                "Date of Birth",
+                "Street",
+                "City",
+                "State",
+                "Zip code",
+              ].map((heading) => (
+                <th key={heading} className="px-4 py-2">
+                  {heading}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee, index) => (
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "bg-white" : "bg-gray-100"} // Permet de changer la couleur de fond en fonction des chiffres paires / impaires
+              >
+                <td className="px-4 py-2">{employee.firstName}</td>
+                <td className="px-4 py-2">{employee.lastName}</td>
+                <td className="px-4 py-2">{employee.startDate}</td>
+                <td className="px-4 py-2">{employee.department}</td>
+                <td className="px-4 py-2">{employee.birthDate}</td>
+                <td className="px-4 py-2">{employee.street}</td>
+                <td className="px-4 py-2">{employee.city}</td>
+                <td className="px-4 py-2">{employee.state}</td>
+                <td className="px-4 py-2">{employee.zipCode}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-between w-full mt-5">
+        <div>Showing 0 to {employees.length} of 0 {employees.length}</div>
+        <div className="flex justify-between w-40">
+          <a href="">Previous</a>
+          <a href="">Next</a>
+        </div>
+      </div>
     </div>
   );
 }
